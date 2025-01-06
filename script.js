58a390a5fa58d72860ce9842325bafe28629d4ae
Kiruthigadurairaj <146357922+Kiruthigadurairaj@users.noreply.github.com>
2025-01-06 17:33:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,24 @@
 function makeid(l) {
-  // write your code here
-var res="";
-	var char_list="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789" 
-	for (var i=0;i<l;i++){
-		res+=char_list.charAt( 
-			Math.floor(
-				Mat.random()*char_list.length
-			)
-	 )
+  // Initialize an empty string to hold the result
+  var res = "";
+
+  // Define the list of characters to use in the random string
+  var char_list = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+  // Loop for the specified length
+  for (var i = 0; i < l; i++) {
+    // Append a random character from char_list to res
+    res += char_list.charAt(
+      Math.floor(
+        Math.random() * char_list.length // Use Math.random (fix typo Mat.random)
+      )
+    );
+  }
+
+  // Return the generated string
+  return res;
 }
+
 
 // Do not change the code below.
 const l = prompt("Enter a number.");
